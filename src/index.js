4044d2e72351ca228b10e8965c4317b2ea066280
Michal Radecki <odrzutowiec@gmail.com>
2018-05-29 22:53:24 +0200

    css renderer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import './index.css';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import * as Three from 'three'
+import { CSS3DObject, CSS3DRenderer } from './CSS3DRenderer'
 
 class App extends React.Component {
   componentDidMount() {
@@ -11,16 +11,18 @@ class App extends React.Component {
     const height = thisDOMNode.clientHeight
 
     this.scene = new Three.Scene()
-    this.renderer = new Three.WebGLRenderer()
+    this.renderer = new CSS3DRenderer()
     this.camera = new Three.PerspectiveCamera(75, width / height, 0.1, 1000)
     this.renderer.setSize(width, height);
     thisDOMNode.appendChild(this.renderer.domElement)
 
-    const geometry = new Three.BoxGeometry(1, 1, 1)
-    const material = new Three.MeshBasicMaterial({ color: 0x00ff00 })
-    const cube = new Three.Mesh(geometry, material)
-    this.scene.add(cube)
-    this.camera.position.z = 5
+    const spriteDOMNode = document.createElement('div')
+    ReactDOM.render('hello', spriteDOMNode)
+    const sprite = new CSS3DObject(spriteDOMNode)
+    sprite.rotateX(Three.Math.degToRad(30))
+    this.scene.add(sprite)
+
+    this.camera.position.z = 20
     this.renderer.render(this.scene, this.camera)
   }
   render() {
